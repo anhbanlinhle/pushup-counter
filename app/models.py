1de2be1e6da0ca73aad9f7f37089cb55287ce75c
anhbanlinhle <vietlinhleviet@gmail.com>
2023-10-23 14:05:25 +0700

    preprocess video to save to server for later process

--- app/models.py
+++ app/models.py
@@ -1,3 +1,20 @@
+import os
+import datetime
+
 def process_video(video_data):
-    # Process the video data here
+    file_name = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+    path = f"../data/{file_name}.mp4"
+    os.makedirs(os.path.dirname(path), exist_ok=True)
+
+    with open(path, "wb") as f:
+        f.write(video_data)
+
+    processed_data = count_pushup(path)
+
     return processed_data
+
+
+def count_pushup(path):
+    count = 0
+    return count
