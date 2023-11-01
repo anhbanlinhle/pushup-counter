0614c72d13b6e24150f987b9907ed2872666eee5
anhbanlinhle <vietlinhleviet@gmail.com>
2023-11-01 20:53:52 +0700

    finish router for squat counter

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, File
-from models import count_pushup, speedup_video
+from models import count_pushup, count_squat, speedup_video
 import os
 import datetime
 
@@ -26,5 +26,15 @@ async def process_video_data(video_data: bytes = File()):
 
 @router.post("/squat")
 async def process_video_squat(video_data: bytes = File()):
-    # task - implement data processing for squat exercise
-    return
+    file_name = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+    old = f"../data/original/{file_name}.mp4"
+    os.makedirs(os.path.dirname(old), exist_ok=True)
+    new = f"../data/speedup/{file_name}.mp4"
+    os.makedirs(os.path.dirname(new), exist_ok=True)
+
+    with open(old, "wb") as f:
+        f.write(video_data)
+
+    speedup_video(old, new)
+    processed_data = count_squat(new)
+    return {"count": processed_data}
